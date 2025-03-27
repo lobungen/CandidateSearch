48a056afe3e41ec7a349934b7149fc108fb1364e
Lee Obungen <lobungen@github.com>
2025-03-27 14:28:41 -0500

    updated candidate interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    avatar_url: string|null;
+    html_url: string|null;
+    login: string|null;
+    id: number;
+    name: string|null;
+    location: string|null;
+    email: string|null;
+    company: string|null;
+    bio: string|null;
+  }
