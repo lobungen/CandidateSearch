13bb52826d0484ccef970fc9a0188176a4e9a022
Lee Obungen <lobungen@github.com>
2025-03-27 14:29:52 -0500

    updated candidate search

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,8 +1,77 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
+import UserCard from '../components/UserCard.tsx';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [currentUser, setCurrentUser] = useState<Candidate>({
+    avatar_url: null,
+    html_url: null,
+    login: null,
+    id: 0,
+    name: null,
+    location: null,
+    email: null,
+    company: null,
+    bio: null,
+  });
+
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [result, setResult] = useState<Candidate[]>([]);
+  const addToCandidateList = (selection:boolean) => {
+    if (selection) {
+    let parsedUsersToCandidate: Candidate[] = [];
+    const storedUsersToCandidate = localStorage.getItem('CandidatesList');
+    if (typeof storedUsersToCandidate === 'string') {
+      parsedUsersToCandidate = JSON.parse(storedUsersToCandidate);
+    }
+    parsedUsersToCandidate.push(currentUser);
+    localStorage.setItem('CandidatesList', JSON.stringify(parsedUsersToCandidate));}
+    
+    if (currentIndex < result.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      setCurrentUser(result[currentIndex + 1]);
+    }
+    else {
+      setCurrentIndex(0);
+      setCurrentUser(result[0]);
+    }
+  };
+
+  const searchForUsers = async () => {
+    const data: Candidate[] = await searchGithub();
+
+    console.log('Data:', data);
+    setResult(data);
+    await searchspecificUser(data[0].login || '');
+  };
+
+  const searchspecificUser = async (username: string) => {
+    const data: Candidate = await searchGithubUser(username);
+    console.log('Data:', data);
+    setCurrentUser(data);
+  };
+
+  useEffect(() => {
+    searchForUsers();
+  }
+    , []);
+
+  return (
+    <>
+      <section id='searchSection'>
+        <h1>Candidate Search</h1>
+
+        <div className='resultsContainer'>
+            <UserCard
+              currentUser={currentUser}
+              
+              addToCandidateList={addToCandidateList}
+            />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default CandidateSearch;
